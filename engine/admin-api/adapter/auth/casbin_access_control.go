konstellation-io/kre

wip replacing check permissions

engine/admin-api/adapter/auth/casbin_access_control.go
@@ -1,10 +1,12 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/casbin/casbin/v2"
 
+	"github.com/konstellation-io/kre/engine/admin-api/delivery/http/token"
 	"github.com/konstellation-io/kre/engine/admin-api/domain/usecase/auth"
 	"github.com/konstellation-io/kre/engine/admin-api/domain/usecase/logging"
 )
@@ -15,41 +17,82 @@ type CasbinAccessControl struct {
 }
 
 func NewCasbinAccessControl(logger logging.Logger, modelPath, policyPath string) (*CasbinAccessControl, error) {
-	e, err := casbin.NewEnforcer(modelPath, policyPath)
+	enforcer, err := casbin.NewEnforcer(modelPath, policyPath)
 	if err != nil {
 		return nil, err
 	}
 
-	accessControl := &CasbinAccessControl{
-		logger,
-		e,
-	}
+	enforcer.AddFunction("isAdmin", isAdminFunc)
+	enforcer.AddFunction("hasGrantForResource", hasGrantsForResourceFunc)
 
-	return accessControl, nil
+	return &CasbinAccessControl{
+		logger,
+		enforcer,
+	}, nil
 }
 
-// change input params to ones obtained from jwt token.
-func (a *CasbinAccessControl) CheckPermission(userID string, resource auth.AccessControlResource, action auth.AccessControlAction) error {
-	if !resource.IsValid() {
-		return invalidAccessControlResourceError
-	}
-
+func (a *CasbinAccessControl) CheckPermission(
+	user *token.UserRoles,
+	resource string,
+	action auth.AccessControlAction,
+) error {
 	if !action.IsValid() {
 		return invalidAccessControlActionError
 	}
 
-	allowed, err := a.enforcer.Enforce(userID, resource.String(), action.String())
-	if err != nil {
-		a.logger.Errorf("error checking permission: %s", err)
-		return err
+	for _, realmRole := range user.RealmAccess.Roles {
+		allowed, err := a.enforcer.Enforce(realmRole, user.ProductRoles, action.String())
+		if err != nil {
+			a.logger.Errorf("error checking permission: %s", err)
+			return err
+		}
+
+		a.logger.Infof(
+			"Checking permission userID[%s] realmRole[%s] action[%s] resource[%s] allowed[%t]",
+			user.UserId, realmRole, action, resource, allowed,
+		)
+
+		if allowed {
+			return nil
+		}
 	}
 
-	a.logger.Infof("Checking permission userID[%s] resource[%s] action[%s] allowed[%t]", userID, resource, action, allowed)
+	//nolint:goerr113 // errors need to be wrapped
+	return fmt.Errorf("you are not allowed to %s %s", action, resource)
+}
 
-	if !allowed {
-		//nolint:goerr113 // errors need to be wrapped
-		return fmt.Errorf("you are not allowed to %s %s", action, resource)
+func hasGrantsForResource(
+	grants map[string][]string,
+	resource,
+	act string,
+) bool {
+	resGrants, ok := grants[resource]
+	if !ok {
+		return false
 	}
 
-	return nil
+	for _, grant := range resGrants {
+		if grant == act {
+			return true
+		}
+	}
+	return false
+}
+
+func hasGrantsForResourceFunc(args ...interface{}) (interface{}, error) {
+	grants := args[0].(map[string][]string)
+	resource := args[1].(string)
+	act := args[2].(string)
+
+	return hasGrantsForResource(grants, resource, act), nil
+}
+
+func isAdmin(role string) bool {
+	return role == "ADMIN"
+}
+
+func isAdminFunc(args ...interface{}) (interface{}, error) {
+	role := args[0].(string)
+
+	return isAdmin(role), nil
 }

engine/admin-api/delivery/http/server.go
@@ -3,9 +3,8 @@ package http
 import (
 	"github.com/konstellation-io/kre/engine/admin-api/adapter/config"
 	"github.com/konstellation-io/kre/engine/admin-api/delivery/http/controller"
-	"github.com/konstellation-io/kre/engine/admin-api/delivery/http/httperrors"
 	kremiddleware "github.com/konstellation-io/kre/engine/admin-api/delivery/http/middleware"
-	"github.com/konstellation-io/kre/engine/admin-api/domain/usecase"
+	"github.com/konstellation-io/kre/engine/admin-api/delivery/http/token"
 	"github.com/konstellation-io/kre/engine/admin-api/domain/usecase/logging"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -26,11 +25,7 @@ const logFormat = "${time_rfc3339} INFO remote_ip=${remote_ip}, method=${method}
 func NewApp(
 	cfg *config.Config,
 	logger logging.Logger,
-	runtimeInteractor *usecase.RuntimeInteractor,
-	userInteractor *usecase.UserInteractor,
-	userActivityInteractor usecase.UserActivityInteracter,
-	versionInteractor *usecase.VersionInteractor,
-	metricsInteractor *usecase.MetricsInteractor,
+	gqlController controller.GraphQL,
 ) *App {
 	e := echo.New()
 	e.HideBanner = true
@@ -53,63 +48,22 @@ func NewApp(
 		}))
 	}
 
-	graphQLController := controller.NewGraphQLController(
-		cfg,
-		logger,
-		runtimeInteractor,
-		userInteractor,
-		userActivityInteractor,
-		versionInteractor,
-		metricsInteractor,
-	)
+	tokenParser := token.NewParser()
 
-	middlewareErrorHandler := func(err error) error {
-		logger.Errorf("Error looking for jwt token: %s", err)
-		return httperrors.HTTPErrUnauthorized
-	}
-
-	skipIfHeaderPresent := func(existCondition bool) func(c echo.Context) bool {
-		return func(c echo.Context) bool {
-			auth := c.Request().Header.Get(echo.HeaderAuthorization)
-			authExists := auth != ""
-
-			return authExists == existCondition
-		}
-	}
-
-	jwtCookieMiddleware := middleware.JWTWithConfig(middleware.JWTConfig{
-		Skipper:      skipIfHeaderPresent(true),
-		SigningKey:   []byte(cfg.Auth.JWTSignSecret),
-		TokenLookup:  "cookie:token",
-		ErrorHandler: middlewareErrorHandler,
-	})
-
-	jwtHeaderMiddleware := middleware.JWTWithConfig(middleware.JWTConfig{
-		Skipper:      skipIfHeaderPresent(false),
-		SigningKey:   []byte(cfg.Auth.JWTSignSecret),
-		ErrorHandler: middlewareErrorHandler,
-	})
-
-	jwtAuthMiddleware := kremiddleware.NewJwtAuthMiddleware(cfg, logger, authInteractor)
-
-	e.POST("/api/v1/auth/signin", authController.SignIn)
-	e.POST("/api/v1/auth/token/signin", authController.SignInWithAPIToken)
-	e.POST("/api/v1/auth/signin/verify", authController.SignInVerify)
-	e.POST("/api/v1/auth/logout", jwtCookieMiddleware(authController.Logout))
+	jwtAuthMiddleware := kremiddleware.NewJwtAuthMiddleware(cfg, logger, tokenParser)
+	e.GET("/", func(c echo.Context) error {
+		return nil
+	}, jwtAuthMiddleware)
 
 	r := e.Group("/graphql")
 	r.Use(jwtAuthMiddleware)
-	r.Use(jwtCookieMiddleware)
-	r.Use(jwtHeaderMiddleware)
-	r.Any("", graphQLController.GraphQLHandler)
-	r.GET("/playground", graphQLController.PlaygroundHandler)
+	r.Any("", gqlController.GraphQLHandler)
+	r.GET("/playground", gqlController.PlaygroundHandler)
 
 	m := e.Group("/measurements")
-	m.Use(jwtCookieMiddleware)
 	m.Use(kremiddleware.ChronografProxy(cfg.Chronograf.Address))
 
 	d := e.Group("/database")
-	d.Use(jwtCookieMiddleware)
 	d.Use(kremiddleware.MongoExpressProxy(cfg.MongoDB.MongoExpressAddress))
 
 	return &App{

engine/admin-api/main.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/konstellation-io/kre/engine/admin-api/adapter/auth"
 	"github.com/konstellation-io/kre/engine/admin-api/adapter/config"
+	"github.com/konstellation-io/kre/engine/admin-api/adapter/product"
 	"github.com/konstellation-io/kre/engine/admin-api/adapter/repository/influx"
 	"github.com/konstellation-io/kre/engine/admin-api/adapter/repository/mongodb"
 	"github.com/konstellation-io/kre/engine/admin-api/adapter/service"
 	"github.com/konstellation-io/kre/engine/admin-api/adapter/version"
 	"github.com/konstellation-io/kre/engine/admin-api/delivery/http"
+	"github.com/konstellation-io/kre/engine/admin-api/delivery/http/controller"
 	"github.com/konstellation-io/kre/engine/admin-api/domain/usecase"
 	"github.com/konstellation-io/kre/engine/admin-api/domain/usecase/logging"
 )
@@ -26,18 +28,25 @@ func main() {
 
 	defer db.Disconnect()
 
-	userActivityInteractor, runtimeInteractor, userInteractor,
+	userActivityInteractor, productInteractor, userInteractor,
 		versionInteractor, metricsInteractor := initApp(cfg, logger, mongodbClient)
 
-	app := http.NewApp(
+	graphqlController := controller.NewGraphQLController(
 		cfg,
 		logger,
-		runtimeInteractor,
+		productInteractor,
 		userInteractor,
 		userActivityInteractor,
 		versionInteractor,
 		metricsInteractor,
 	)
+
+	app := http.NewApp(
+		cfg,
+		logger,
+		graphqlController,
+	)
+
 	app.Start()
 }
 
@@ -70,10 +79,10 @@ func initApp(cfg *config.Config, logger logging.Logger, mongodbClient *mongo.Cli
 
 	userActivityInteractor := usecase.NewUserActivityInteractor(logger, userActivityRepo, accessControl)
 
-	runtimeInteractor := usecase.NewRuntimeInteractor(
+	runtimeInteractor := usecase.NewProductInteractor(
 		cfg,
 		logger,
-		runtimeRepo,
+		productRepo,
 		measurementRepo,
 		versionMongoRepo,
 		metricRepo,
@@ -93,7 +102,7 @@ func initApp(cfg *config.Config, logger logging.Logger, mongodbClient *mongo.Cli
 		cfg,
 		logger,
 		versionMongoRepo,
-		runtimeRepo,
+		productRepo,
 		versionService,
 		natsManagerService,
 		userActivityInteractor,
@@ -106,10 +115,10 @@ func initApp(cfg *config.Config, logger logging.Logger, mongodbClient *mongo.Cli
 
 	metricsInteractor := usecase.NewMetricsInteractor(
 		logger,
-		runtimeRepo,
+		productRepo,
 		accessControl,
 		metricRepo,
 	)
 
-	return userActivityInteractor, runtimeInteractor, userInteractor, versionInteractor, metricsInteractor
+	return userActivityInteractor, productInteractor, userInteractor, versionInteractor, metricsInteractor
 }

konstellation-io/kai

engine/admin-api/adapter/auth/casbin_access_control.go
@@ -1,10 +1,12 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/casbin/casbin/v2"
 
+	"github.com/konstellation-io/kai/engine/admin-api/delivery/http/token"
 	"github.com/konstellation-io/kai/engine/admin-api/domain/usecase/auth"
 	"github.com/konstellation-io/kai/engine/admin-api/domain/usecase/logging"
 )
@@ -15,41 +17,82 @@ type CasbinAccessControl struct {
 }
 
 func NewCasbinAccessControl(logger logging.Logger, modelPath, policyPath string) (*CasbinAccessControl, error) {
-	e, err := casbin.NewEnforcer(modelPath, policyPath)
+	enforcer, err := casbin.NewEnforcer(modelPath, policyPath)
 	if err != nil {
 		return nil, err
 	}
 
-	accessControl := &CasbinAccessControl{
-		logger,
-		e,
-	}
+	enforcer.AddFunction("isAdmin", isAdminFunc)
+	enforcer.AddFunction("hasGrantForResource", hasGrantsForResourceFunc)
 
-	return accessControl, nil
+	return &CasbinAccessControl{
+		logger,
+		enforcer,
+	}, nil
 }
 
-// change input params to ones obtained from jwt token.
-func (a *CasbinAccessControl) CheckPermission(userID string, resource auth.AccessControlResource, action auth.AccessControlAction) error {
-	if !resource.IsValid() {
-		return invalidAccessControlResourceError
-	}
-
+func (a *CasbinAccessControl) CheckPermission(
+	user *token.UserRoles,
+	resource string,
+	action auth.AccessControlAction,
+) error {
 	if !action.IsValid() {
 		return invalidAccessControlActionError
 	}
 
-	allowed, err := a.enforcer.Enforce(userID, resource.String(), action.String())
-	if err != nil {
-		a.logger.Errorf("error checking permission: %s", err)
-		return err
+	for _, realmRole := range user.RealmAccess.Roles {
+		allowed, err := a.enforcer.Enforce(realmRole, user.ProductRoles, action.String())
+		if err != nil {
+			a.logger.Errorf("error checking permission: %s", err)
+			return err
+		}
+
+		a.logger.Infof(
+			"Checking permission userID[%s] realmRole[%s] action[%s] resource[%s] allowed[%t]",
+			user.UserId, realmRole, action, resource, allowed,
+		)
+
+		if allowed {
+			return nil
+		}
 	}
 
-	a.logger.Infof("Checking permission userID[%s] resource[%s] action[%s] allowed[%t]", userID, resource, action, allowed)
+	//nolint:goerr113 // errors need to be wrapped
+	return fmt.Errorf("you are not allowed to %s %s", action, resource)
+}
 
-	if !allowed {
-		//nolint:goerr113 // errors need to be wrapped
-		return fmt.Errorf("you are not allowed to %s %s", action, resource)
+func hasGrantsForResource(
+	grants map[string][]string,
+	resource,
+	act string,
+) bool {
+	resGrants, ok := grants[resource]
+	if !ok {
+		return false
 	}
 
-	return nil
+	for _, grant := range resGrants {
+		if grant == act {
+			return true
+		}
+	}
+	return false
+}
+
+func hasGrantsForResourceFunc(args ...interface{}) (interface{}, error) {
+	grants := args[0].(map[string][]string)
+	resource := args[1].(string)
+	act := args[2].(string)
+
+	return hasGrantsForResource(grants, resource, act), nil
+}
+
+func isAdmin(role string) bool {
+	return role == "ADMIN"
+}
+
+func isAdminFunc(args ...interface{}) (interface{}, error) {
+	role := args[0].(string)
+
+	return isAdmin(role), nil
 }

engine/admin-api/delivery/http/server.go
@@ -3,9 +3,8 @@ package http
 import (
 	"github.com/konstellation-io/kai/engine/admin-api/adapter/config"
 	"github.com/konstellation-io/kai/engine/admin-api/delivery/http/controller"
-	"github.com/konstellation-io/kai/engine/admin-api/delivery/http/httperrors"
-	kaimiddleware "github.com/konstellation-io/kai/engine/admin-api/delivery/http/middleware"
-	"github.com/konstellation-io/kai/engine/admin-api/domain/usecase"
+	kaimiddleware "github.com/konstellation-io/kre/engine/admin-api/delivery/http/middleware"
+	"github.com/konstellation-io/kai/engine/admin-api/delivery/http/token"
 	"github.com/konstellation-io/kai/engine/admin-api/domain/usecase/logging"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -26,11 +25,7 @@ const logFormat = "${time_rfc3339} INFO remote_ip=${remote_ip}, method=${method}
 func NewApp(
 	cfg *config.Config,
 	logger logging.Logger,
-	runtimeInteractor *usecase.RuntimeInteractor,
-	userInteractor *usecase.UserInteractor,
-	userActivityInteractor usecase.UserActivityInteracter,
-	versionInteractor *usecase.VersionInteractor,
-	metricsInteractor *usecase.MetricsInteractor,
+	gqlController controller.GraphQL,
 ) *App {
 	e := echo.New()
 	e.HideBanner = true
@@ -53,63 +48,22 @@ func NewApp(
 		}))
 	}
 
-	graphQLController := controller.NewGraphQLController(
-		cfg,
-		logger,
-		runtimeInteractor,
-		userInteractor,
-		userActivityInteractor,
-		versionInteractor,
-		metricsInteractor,
-	)
+	tokenParser := token.NewParser()
 
-	middlewareErrorHandler := func(err error) error {
-		logger.Errorf("Error looking for jwt token: %s", err)
-		return httperrors.HTTPErrUnauthorized
-	}
-
-	skipIfHeaderPresent := func(existCondition bool) func(c echo.Context) bool {
-		return func(c echo.Context) bool {
-			auth := c.Request().Header.Get(echo.HeaderAuthorization)
-			authExists := auth != ""
-
-			return authExists == existCondition
-		}
-	}
-
-	jwtCookieMiddleware := middleware.JWTWithConfig(middleware.JWTConfig{
-		Skipper:      skipIfHeaderPresent(true),
-		SigningKey:   []byte(cfg.Auth.JWTSignSecret),
-		TokenLookup:  "cookie:token",
-		ErrorHandler: middlewareErrorHandler,
-	})
-
-	jwtHeaderMiddleware := middleware.JWTWithConfig(middleware.JWTConfig{
-		Skipper:      skipIfHeaderPresent(false),
-		SigningKey:   []byte(cfg.Auth.JWTSignSecret),
-		ErrorHandler: middlewareErrorHandler,
-	})
-
-	jwtAuthMiddleware := kremiddleware.NewJwtAuthMiddleware(cfg, logger, authInteractor)
-
-	e.POST("/api/v1/auth/signin", authController.SignIn)
-	e.POST("/api/v1/auth/token/signin", authController.SignInWithAPIToken)
-	e.POST("/api/v1/auth/signin/verify", authController.SignInVerify)
-	e.POST("/api/v1/auth/logout", jwtCookieMiddleware(authController.Logout))
+	jwtAuthMiddleware := kremiddleware.NewJwtAuthMiddleware(cfg, logger, tokenParser)
+	e.GET("/", func(c echo.Context) error {
+		return nil
+	}, jwtAuthMiddleware)
 
 	r := e.Group("/graphql")
 	r.Use(jwtAuthMiddleware)
-	r.Use(jwtCookieMiddleware)
-	r.Use(jwtHeaderMiddleware)
-	r.Any("", graphQLController.GraphQLHandler)
-	r.GET("/playground", graphQLController.PlaygroundHandler)
+	r.Any("", gqlController.GraphQLHandler)
+	r.GET("/playground", gqlController.PlaygroundHandler)
 
 	m := e.Group("/measurements")
-	m.Use(jwtCookieMiddleware)
 	m.Use(kaimiddleware.ChronografProxy(cfg.Chronograf.Address))
 
 	d := e.Group("/database")
-	d.Use(jwtCookieMiddleware)
 	d.Use(kaimiddleware.MongoExpressProxy(cfg.MongoDB.MongoExpressAddress))
 
 	return &App{

engine/admin-api/main.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/konstellation-io/kai/engine/admin-api/adapter/auth"
 	"github.com/konstellation-io/kai/engine/admin-api/adapter/config"
+	"github.com/konstellation-io/kai/engine/admin-api/adapter/product"
 	"github.com/konstellation-io/kai/engine/admin-api/adapter/repository/influx"
 	"github.com/konstellation-io/kai/engine/admin-api/adapter/repository/mongodb"
 	"github.com/konstellation-io/kai/engine/admin-api/adapter/service"
 	"github.com/konstellation-io/kai/engine/admin-api/adapter/version"
 	"github.com/konstellation-io/kai/engine/admin-api/delivery/http"
+	"github.com/konstellation-io/kai/engine/admin-api/delivery/http/controller"
 	"github.com/konstellation-io/kai/engine/admin-api/domain/usecase"
 	"github.com/konstellation-io/kai/engine/admin-api/domain/usecase/logging"
 )
@@ -26,18 +28,25 @@ func main() {
 
 	defer db.Disconnect()
 
-	userActivityInteractor, runtimeInteractor, userInteractor,
+	userActivityInteractor, productInteractor, userInteractor,
 		versionInteractor, metricsInteractor := initApp(cfg, logger, mongodbClient)
 
-	app := http.NewApp(
+	graphqlController := controller.NewGraphQLController(
 		cfg,
 		logger,
-		runtimeInteractor,
+		productInteractor,
 		userInteractor,
 		userActivityInteractor,
 		versionInteractor,
 		metricsInteractor,
 	)
+
+	app := http.NewApp(
+		cfg,
+		logger,
+		graphqlController,
+	)
+
 	app.Start()
 }
 
@@ -70,10 +79,10 @@ func initApp(cfg *config.Config, logger logging.Logger, mongodbClient *mongo.Cli
 
 	userActivityInteractor := usecase.NewUserActivityInteractor(logger, userActivityRepo, accessControl)
 
-	runtimeInteractor := usecase.NewRuntimeInteractor(
+	runtimeInteractor := usecase.NewProductInteractor(
 		cfg,
 		logger,
-		runtimeRepo,
+		productRepo,
 		measurementRepo,
 		versionMongoRepo,
 		metricRepo,
@@ -93,7 +102,7 @@ func initApp(cfg *config.Config, logger logging.Logger, mongodbClient *mongo.Cli
 		cfg,
 		logger,
 		versionMongoRepo,
-		runtimeRepo,
+		productRepo,
 		versionService,
 		natsManagerService,
 		userActivityInteractor,
@@ -106,10 +115,10 @@ func initApp(cfg *config.Config, logger logging.Logger, mongodbClient *mongo.Cli
 
 	metricsInteractor := usecase.NewMetricsInteractor(
 		logger,
-		runtimeRepo,
+		productRepo,
 		accessControl,
 		metricRepo,
 	)
 
-	return userActivityInteractor, runtimeInteractor, userInteractor, versionInteractor, metricsInteractor
+	return userActivityInteractor, productInteractor, userInteractor, versionInteractor, metricsInteractor
 }