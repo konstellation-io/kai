ad3f6856790df73203f95f1830d73cce2873eae9
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-09-12 17:48:39 +0200

    feat: add test coverage for missing methods

---
+++ engine/admin-api/domain/usecase/version/get_by_id_test.go
@@ -0,0 +1,20 @@
+//go:build unit
+
+package version_test
+
+import (
+	"github.com/konstellation-io/kai/engine/admin-api/domain/usecase/version/utils"
+)
+
+func (s *VersionUsecaseTestSuite) TestGetByID() {
+	// GIVEN a productID and an ID
+	productID := "product-1"
+	testVersion := utils.InitTestVersion().WithVersionID("test-ID").GetVersion()
+
+	s.versionRepo.EXPECT().GetByID(productID, testVersion.ID).Return(testVersion, nil)
+
+	actual, err := s.versionRepo.GetByID(productID, testVersion.ID)
+	s.Require().NoError(err)
+
+	s.Equal(testVersion, actual)
+}
