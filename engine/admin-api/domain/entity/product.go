9c577bd0052603bb99173f663f13bb360f195e15
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-09-20 12:17:39 +0200

    fix: linter

--- engine/admin-api/domain/entity/product.go
+++ engine/admin-api/domain/entity/product.go
@@ -12,11 +12,11 @@ var (
 )
 
 type Product struct {
-	ID               string    `bson:"_id" validate:"required"`
-	Name             string    `bson:"name" validate:"required,lte=40"`
-	Description      string    `bson:"description" validate:"required,lte=500"`
-	CreationDate     time.Time `bson:"creationDate"`
-	Owner            string    `bson:"owner"`
+	ID           string    `bson:"_id" validate:"required"`
+	Name         string    `bson:"name" validate:"required,lte=40"`
+	Description  string    `bson:"description" validate:"required,lte=500"`
+	CreationDate time.Time `bson:"creationDate"`
+	Owner        string    `bson:"owner"`
 }
 
 func (r *Product) Validate() error {
