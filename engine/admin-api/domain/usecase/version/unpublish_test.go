701eaf34f2cfac8c9ebd0eadf602989bffedd7c5
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2023-12-04 14:05:31 +0100

    update tests

--- engine/admin-api/domain/usecase/version/unpublish_test.go
+++ engine/admin-api/domain/usecase/version/unpublish_test.go
@@ -21,12 +21,20 @@ func (s *versionSuite) TestUnpublish_OK() {
 		WithTag(versionTag).
 		WithStatus(entity.VersionStatusPublished).
 		Build()
+	product := testhelpers.NewProductBuilder().
+		WithPublishedVersion(&vers.Tag).
+		Build()
 
 	s.accessControl.EXPECT().CheckProductGrants(user, productID, auth.ActUnpublishVersion).Return(nil)
 	s.versionRepo.EXPECT().GetByTag(ctx, productID, versionTag).Return(vers, nil)
+	s.productRepo.EXPECT().GetByID(ctx, productID).Return(product, nil)
 
 	s.versionService.EXPECT().Unpublish(ctx, productID, vers).Return(nil)
 	s.versionRepo.EXPECT().Update(productID, vers).Return(nil)
+	s.productRepo.EXPECT().Update(ctx, product).DoAndReturn(func(ctx context.Context, product *entity.Product) error {
+		s.Nil(product.PublishedVersion)
+		return nil
+	})
 	s.userActivityInteractor.EXPECT().RegisterUnpublishAction(user.Email, productID, vers, "unpublishing").Return(nil)
 
 	// WHEN unpublishing the version
@@ -91,6 +99,29 @@ func (s *versionSuite) TestUnpublish_ErrorVersionCannotBeUnpublished() {
 	s.ErrorIs(err, version.ErrVersionCannotBeUnpublished)
 }
 
+func (s *versionSuite) TestUnpublish_ErrorProductNotFound() {
+	// GIVEN a valid user and a published version, but product not found
+	ctx := context.Background()
+	user := testhelpers.NewUserBuilder().Build()
+	vers := testhelpers.NewVersionBuilder().
+		WithTag(versionTag).
+		WithStatus(entity.VersionStatusPublished).
+		Build()
+
+	s.accessControl.EXPECT().CheckProductGrants(user, productID, auth.ActUnpublishVersion).Return(nil)
+	s.versionRepo.EXPECT().GetByTag(ctx, productID, versionTag).Return(vers, nil)
+	s.productRepo.EXPECT().GetByID(ctx, productID).Return(nil, fmt.Errorf("no product found"))
+
+	s.versionService.EXPECT().Unpublish(ctx, productID, vers).Return(nil)
+	s.versionRepo.EXPECT().Update(productID, vers).Return(nil)
+
+	// WHEN unpublishing the version
+	_, err := s.handler.Unpublish(ctx, user, productID, versionTag, "unpublishing")
+
+	// THEN an error is returned
+	s.Error(err)
+}
+
 func (s *versionSuite) TestUnpublish_ErrorUnpublishingVersion() {
 	// GIVEN a valid user and a published version, but error during unpublishing
 	ctx := context.Background()
@@ -121,25 +152,34 @@ func (s *versionSuite) TestUnpublish_CheckNonBlockingErrorLogging() {
 		WithTag(versionTag).
 		WithStatus(entity.VersionStatusPublished).
 		Build()
+	product := testhelpers.NewProductBuilder().
+		WithPublishedVersion(&vers.Tag).
+		Build()
 
 	setStatusErr := errors.New("error updating version status")
+	updateProductErr := errors.New("error updating product published version")
 	registerActionErr := errors.New("error registering unpublish action")
 
 	s.accessControl.EXPECT().CheckProductGrants(user, productID, auth.ActUnpublishVersion).Return(nil)
 	s.versionRepo.EXPECT().GetByTag(ctx, productID, versionTag).Return(vers, nil)
+	s.productRepo.EXPECT().GetByID(ctx, productID).Return(product, nil)
 
 	s.versionService.EXPECT().Unpublish(ctx, productID, vers).Return(nil)
 	s.versionRepo.EXPECT().Update(productID, vers).Return(setStatusErr)
+	s.productRepo.EXPECT().Update(ctx, product).Return(updateProductErr)
 	s.userActivityInteractor.EXPECT().RegisterUnpublishAction(user.Email, productID, vers, "unpublishing").Return(registerActionErr)
 
 	// WHEN unpublishing the version
 	_, err := s.handler.Unpublish(ctx, user, productID, versionTag, "unpublishing")
 	s.NoError(err)
 
-	s.Require().Len(s.observedLogs.All(), 3)
+	s.Require().Len(s.observedLogs.All(), 4)
 	print(s.observedLogs.All())
 	log1 := s.observedLogs.All()[1]
 	s.Equal(log1.ContextMap()["error"], setStatusErr.Error())
 	log2 := s.observedLogs.All()[2]
-	s.Equal(log2.ContextMap()["error"], registerActionErr.Error())
+	s.Equal(log2.ContextMap()["error"], updateProductErr.Error())
+	log3 := s.observedLogs.All()[3]
+	s.Equal(log3.ContextMap()["error"], registerActionErr.Error())
+
 }
