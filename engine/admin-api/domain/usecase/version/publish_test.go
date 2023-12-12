32a0c2dbe340ed1576a9207f36bd2fefe5cf58bf
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2023-12-12 11:02:28 +0000

    undo some changes

--- engine/admin-api/domain/usecase/version/publish_test.go
+++ engine/admin-api/domain/usecase/version/publish_test.go
@@ -25,9 +25,6 @@ func (s *versionSuite) TestPublish_OK() {
 		Build()
 	product := testhelpers.NewProductBuilder().Build()
 
-	product2 := testhelpers.NewProductBuilder().Build()
-	product2.PublishedVersion = &vers.Tag
-
 	expectedURLs := map[string]string{
 		"test-trigger": "test-url",
 	}
@@ -38,7 +35,7 @@ func (s *versionSuite) TestPublish_OK() {
 
 	s.versionService.EXPECT().Publish(ctx, product.ID, vers.Tag).Return(expectedURLs, nil)
 	s.versionRepo.EXPECT().Update(product.ID, vers).Return(nil)
-	s.productRepo.EXPECT().Update(ctx, product2).Return(nil)
+	s.productRepo.EXPECT().Update(ctx, product).Return(nil)
 	s.userActivityInteractor.EXPECT().RegisterPublishAction(user.Email, product.ID, vers, "publishing").Return(nil)
 
 	// WHEN publishing the version
