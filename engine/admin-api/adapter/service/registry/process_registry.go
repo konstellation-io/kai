b1cadb6374beaa9d12f09d283ae8718b13715d92
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2024-02-13 18:40:29 +0100

    linter set me free

--- engine/admin-api/adapter/service/registry/process_registry.go
+++ engine/admin-api/adapter/service/registry/process_registry.go
@@ -26,30 +26,36 @@ func NewProcessRegistry() *ProcessRegistry {
 	return &ProcessRegistry{}
 }
 
-func (c ProcessRegistry) DeleteProcess(ctx context.Context, imageName, version string) error {
-	registryHost := viper.GetString(config.RegistryHostKey)
-	authSecret := viper.GetString(config.RegistryAuthSecretKey)
-
+func formURL(registryHost, imageName, joker string) string {
 	if !strings.HasPrefix(registryHost, "http") {
 		registryHost = "http://" + registryHost
 	}
 
+	return registryHost + "/v2/" + imageName + "/manifests/" + joker
+}
+
+func (c ProcessRegistry) DeleteProcess(ctx context.Context, imageName, version string) error {
+	registryHost := viper.GetString(config.RegistryHostKey)
+	authSecret := viper.GetString(config.RegistryAuthSecretKey)
+	basicAuthHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte(authSecret))
+
 	client := &http.Client{}
 
-	req, err := http.NewRequestWithContext(ctx, "GET", registryHost+"/v2/"+imageName+"/manifests/"+version, http.NoBody)
+	url := formURL(registryHost, imageName, version)
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, http.NoBody)
 	if err != nil {
 		return err
 	}
 
-	basicAuth := base64.StdEncoding.EncodeToString([]byte(authSecret))
-
-	req.Header.Add("Authorization", "Basic "+basicAuth)
+	req.Header.Add("Authorization", basicAuthHeader)
 	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v2+json")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
+
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
@@ -58,19 +64,20 @@ func (c ProcessRegistry) DeleteProcess(ctx context.Context, imageName, version s
 
 	digest := resp.Header.Get("Docker-Content-Digest")
 
-	req, err = http.NewRequestWithContext(ctx, "DELETE", registryHost+"/v2/"+imageName+"/manifests/"+digest, http.NoBody)
+	url = formURL(registryHost, imageName, digest)
+
+	req, err = http.NewRequestWithContext(ctx, "DELETE", url, http.NoBody)
 	if err != nil {
 		return err
 	}
 
-	req.Header.Add("Authorization", "Basic "+basicAuth)
-
-	req.Body = http.NoBody
+	req.Header.Add("Authorization", basicAuthHeader)
 
 	resp, err = client.Do(req)
 	if err != nil {
 		return err
 	}
+
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusAccepted {
